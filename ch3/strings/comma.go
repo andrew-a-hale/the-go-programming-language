aba49d250f140a347df6479a658bcd319f45388a
andrew-a-hale <andrew.hale003@gmail.com>
2023-08-27 12:14:43 +0000

    added validation to recurisve comma

--- ch3/strings/comma.go
+++ ch3/strings/comma.go
@@ -3,6 +3,9 @@ package main
 
 import (
 	"bytes"
+	"errors"
+	"fmt"
+	"regexp"
 	"strings"
 )
 
@@ -17,6 +20,10 @@ func main() {
 	println(buffercomma("1234.133452"))
 	println(buffercomma("-123422.12"))
 	println(buffercomma("-12.12"))
+	println(buffercomma(""))
+	println(buffercomma("abcd"))
+	println(buffercomma("abc1"))
+	println(buffercomma("123,123.2"))
 }
 
 func recursivecomma(s string) string {
@@ -27,18 +34,30 @@ func recursivecomma(s string) string {
 	return recursivecomma(s[:n-3]) + "," + s[n-3:]
 }
 
-func buffercomma(s string) string {
+func buffercomma(s string) (string, error) {
 	var buf bytes.Buffer
-	var n int
+	var size int // number of digits before a decimal point, otherwise len(s)
 
-	if strings.Contains(s, ".") {
-		n = len(s[:strings.Index(s, ".")])
-	} else {
-		n = len(s)
+	// validate
+	regex := `^[\d-+\.]*$`
+	matched, err := regexp.Match(regex, []byte(s))
+	switch {
+	case len(s) == 0:
+		return "", errors.New(fmt.Sprint("received empty string\n"))
+	case err != nil:
+		return "", errors.New(fmt.Sprintf("invalid regex used, regex was: %v", regex))
+	case !matched:
+		return "", errors.New(fmt.Sprintf("string contained invalid characters expected decimal-like strings, got: %v\n", s))
 	}
 
+	size = strings.LastIndex(s, ".")
+	if size == -1 {
+		size = len(s)
+	}
+
+	// peel off the +/- and write to buf
 	if strings.HasPrefix(s, "+") || strings.HasPrefix(s, "-") {
-		n -= 1
+		size -= 1
 		buf.WriteByte(s[0])
 		s = s[1:]
 	}
@@ -46,12 +65,12 @@ func buffercomma(s string) string {
 	for i, j := range s {
 		if j == '.' {
 			buf.WriteString(s[i:])
-			return buf.String()
+			return buf.String(), nil
 		}
-		if (n-i)%3 == 0 && i > 0 {
+		if (size-i)%3 == 0 && i > 0 {
 			buf.WriteRune(',')
 		}
 		buf.WriteRune(j)
 	}
-	return buf.String()
+	return buf.String(), nil
 }
