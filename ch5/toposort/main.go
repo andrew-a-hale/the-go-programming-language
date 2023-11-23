cfe1d7b03c50395b1cf7d3c64bbdfa62104ded03
Andrew Hale <andrew.hale003@gmail.com>
2023-11-23 11:39:49 +0000

    5.14 complete

--- ch5/toposort/main.go
+++ ch5/toposort/main.go
@@ -8,7 +8,6 @@ import (
 var prereqs = map[string][]string{
 	"algorithms":            {"data structures"},
 	"calculus":              {"linear algebra"},
-	"linear algebra":        {"calculus 2"},
 	"calculus 2":            {"calculus"},
 	"compilers":             {"data structures", "formal languages", "computer organization"},
 	"data structures":       {"discrete math"},
@@ -16,12 +15,20 @@ var prereqs = map[string][]string{
 	"discrete math":         {"intro to programming"},
 	"formal languages":      {"discrete math"},
 	"networks":              {"operating systems"},
-	"operatings systems":    {"data structures", "computer organization"},
+	"operating systems":     {"data structures", "computer organization"},
 	"programming languages": {"data structures", "computer organization"},
 }
 
+var files = map[string][]string{
+	"d1": {"d4", "f2", "f3"},
+	"d4": {"f10", "f11"},
+	"d5": {"f1", "f5"},
+	"d2": {"f4", "d5", "f6"},
+	"d3": {"f7", "f8", "f9"},
+}
+
 func main() {
-	ordering := topoSort(prereqs)
+	ordering := topoSortDepth(files)
 	for i := 1; i < len(ordering); i++ {
 		val, err := getByValue(ordering, i)
 		if err != nil {
@@ -29,9 +36,48 @@ func main() {
 		}
 		fmt.Printf("%d:\t%s\n", i, val)
 	}
+
+	topoSortBreadth(files)
 }
 
-func topoSort(m map[string][]string) map[string]int {
+func topoSortBreadth(m map[string][]string) {
+	seen := make(map[string]bool)
+
+	// search prereqs
+	var f func(item string) []string = func(item string) []string {
+		if !seen[item] {
+			fmt.Println(item)
+		}
+		for _, el := range m[item] {
+			if !seen[el] {
+				seen[el] = true
+				fmt.Println(el)
+			}
+		}
+		return m[item]
+	}
+
+	// invert map
+	inverted := make(map[string][]string)
+	var worklist []string
+	for k := range m {
+		for _, v := range m[k] {
+			inverted[v] = append(inverted[v], k)
+		}
+		worklist = append(worklist, k)
+	}
+
+	// breadthfirst
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			worklist = append(worklist, f(item)...)
+		}
+	}
+}
+
+func topoSortDepth(m map[string][]string) map[string]int {
 	var visitAll func(item string)
 	order := make(map[string]int)
 	var pos int
